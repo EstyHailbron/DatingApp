084a873355ea7f9b53caed35228916d3d73f9fff
EstyHailbron <esty7605@gmail.com>
2023-12-26 17:11:49 +0200

    26/12/2023

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -9,17 +9,5 @@ public class DataContext :DbContext
     {
     }
 
-    public DbSet<AppUser> Users { get; set; }
-
-    // protected override void OnModelCreating(ModelBuilder modelBuilder)
-    // {
-    //     base.OnModelCreating(modelBuilder);
-
-    //     modelBuilder.Entity<AppUser>().HasData(
-    //         new AppUser {Id = 1, UserName = "Bob"},
-    //         new AppUser {Id = 2, UserName = "Alice"},
-    //         new AppUser {Id = 3, UserName = "Tom"}
-    //     );
-    // }
-
+    public DbSet<AppUser> Users { get; set; };
 }
