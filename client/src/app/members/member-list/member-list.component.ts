89a19726217d28ed14c73086fc0900d4758a8d65
EstyHailbron <esty7605@gmail.com>
2024-01-16 18:45:32 +0200

    Building the user interface

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { MemberService } from 'src/app/_services/member.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css']
+  styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
+  members: Member[] = [];
+  member: Member | undefined;
 
-  constructor() { }
+  constructor(private memberService: MemberService) {}
 
   ngOnInit(): void {
+    this.loadMembers();
   }
 
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: (members) => (this.members = members),
+    });
+  }
+
+  loadMember(username: string) {
+    this.memberService.getMember(username).subscribe((member) => {
+      this.member = member;
+    });
+  }
 }
